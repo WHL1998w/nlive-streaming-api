80d2347c60987891122bbcdd4c1c9c48b8c99e91
wanghuanle <wanghuanle@qq.com>
2020-11-01 15:50:47 +0800

    关闭csrf，开启跨域

--- config/plugin.js
+++ config/plugin.js
@@ -2,8 +2,9 @@
 
 /** @type Egg.EggPlugin */
 module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
+  // 配置跨域插件
+  cors: {
+    enable: true,
+    package: 'egg-cors',
+  },
 };
